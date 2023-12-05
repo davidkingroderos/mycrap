7a4cd6e2e71e4db58fb9c856375d4011b0ad8b5b
David King Roderos <iamdavidkingroderos@gmail.com>
2023-12-06 02:32:52 +0800

    2D block rendered

--- src/MyCrap/main.cpp
+++ src/MyCrap/main.cpp
@@ -58,24 +58,84 @@ int main()
 		return -1;
 	}
 
-	Shader dirtShader("vert.shader", "frag.shader");
+	Shader shader("vert.shader", "frag.shader");
 
 	float cube[] = {
 		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,
-		 0.5f, -0.5f,  0.5f,  
-		-0.5f,  0.5f,  0.5f, 
-		 0.5f,  0.5f,  0.5f,
+		-0.5f,  0.5f,  0.5f,  0.0f,  1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f,  1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f,  0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f,  0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f,  1.0f,
 	};
+
+	GLuint VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cube), cube, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	GLuint textures[1];
+	glGenTextures(1, textures);
+
+	glBindTexture(GL_TEXTURE_2D, textures[0]);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	int width, height, nrChannels;
+
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char* data = stbi_load("sidegrass.jpg", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load container texture" << std::endl;
+	}
+	stbi_image_free(data);
 
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput(window);
 
+		glClearColor(0.0f, 1.0f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, textures[0]);
+
+		shader.use();
+
+		glBindVertexArray(VAO);
+
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+
 	glfwTerminate();
+
+	return 0;
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
--- src/MyCrap/vert.shader
+++ src/MyCrap/vert.shader
@@ -1,7 +1,7 @@
 #version 330 core
 
 layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec3 aTexCoord;
+layout (location = 1) in vec2 aTexCoord;
 
 out vec2 TexCoord;
 
@@ -12,4 +12,5 @@ uniform mat4 model;
 void main()
 {
 	gl_Position = vec4(aPos, 1.0);
+	TexCoord = aTexCoord;
 }
